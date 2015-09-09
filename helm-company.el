9433d612b77650fc0b49d6f37ea63f135416d338
Manuel <manuel@boccaperta.com>
2015-09-09 10:10:58 +0200

    Require helm-multi-match
    Fixes this issue: https://github.com/yasuyk/helm-company/issues/20

--- helm-company.el
+++ helm-company.el
@@ -33,7 +33,7 @@
 ;;; Code:
 
 (require 'helm)
-(require 'helm-match-plugin)
+(require 'helm-multi-match)
 (require 'helm-files)
 (require 'helm-elisp) ;; For with-helm-show-completion
 (require 'company)
